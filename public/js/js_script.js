3b52c847ef213cf8c584470d0430fd6bd2b0b742
David Wiberg <David.Wiberg.9302@student.uu.se>
2020-02-01 15:34:42 +0100

    Task 4-3 completed

---
+++ public/js/js_script.js
@@ -0,0 +1,23 @@
+function menuItem(name,kcal, gluten, lactose, src){
+    this.name = name;
+    this.kCal = kcal;
+    this.gluten = gluten;
+    this.lactose = lactose;
+    this.img = src;
+    this.info = function() {
+	 return this.name + ' ' + this.kCal; 
+    }
+}
+
+let item = new menuItem('FireBurger', '700', 'gluten', 'lactose', 'https://www.mamoli.se/images/meny/hamburgare.png');
+let item2 = new menuItem('LegendBurger', '600', 'no', 'no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item3 = new menuItem('EpicBurger', '1000', 'gluten','no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item4 = new menuItem('Burger', '800', 'no', 'no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item5 = new menuItem('BurgerSki', '900', 'gluten','no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+
+console.log(item.info());
+console.log(item2.info());
+console.log(item3.info());
+console.log(item4.info());
+console.log(item5.info());
+
