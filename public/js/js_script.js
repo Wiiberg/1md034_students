ed6c6bd61dce1ea9e10b848631023dd3e6c40899
David Wiberg <David.Wiberg.9302@student.uu.se>
2020-02-03 14:24:02 +0100

    Task 4-6 completed

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -15,6 +15,12 @@ let item3 = new menuItem('EpicBurger', '1000', 'gluten','no', 'https://www.magnu
 let item4 = new menuItem('Burger', '800', 'no', 'no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
 let item5 = new menuItem('BurgerSki', '900', 'gluten','no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
 
+document.getElementById("burger").innerHTML = item.info();
+document.getElementById("burger2").innerHTML = item2.info();
+document.getElementById("burger3").innerHTML = item3.info();
+document.getElementById("burger4").innerHTML = item4.info();
+document.getElementById("burger5").innerHTML = item5.info();
+
 console.log(item.info());
 console.log(item2.info());
 console.log(item3.info());
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,11 +1,17 @@
 window.onload = function(){
 
-const vm = new Vue({
-  el: '#myID',
-  data: {
-    arbitraryVariableName: 'Välj en burgare'
-  }
-});
 
-
+    const vm = new Vue({
+	el: '#myID',
+	data: {
+	    item: item.info(),
+	    item2: item2.info(),
+	    item3: item3.info(),
+	    item4: item4.info(),
+	    item5: item5.info()
+    
+    
+   }
+   
+})
 }
