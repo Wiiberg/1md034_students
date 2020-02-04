c37b07ae0031968bf9850bb5dd3031c96f432a19
David Wiberg <David.Wiberg.9302@student.uu.se>
2020-02-04 13:39:08 +0100

    Task 4-7 completed

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,29 +1,36 @@
-function menuItem(name,kcal, gluten, lactose, src){
+function menuItem(name,kcal, allergi, src){
     this.name = name;
     this.kCal = kcal;
-    this.gluten = gluten;
-    this.lactose = lactose;
+    this.allergis = allergi; 
     this.img = src;
     this.info = function() {
 	 return this.name + ' ' + this.kCal; 
     }
 }
 
-let item = new menuItem('FireBurger', '700', 'gluten', 'lactose', 'https://www.mamoli.se/images/meny/hamburgare.png');
-let item2 = new menuItem('LegendBurger', '600', 'no', 'no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
-let item3 = new menuItem('EpicBurger', '1000', 'gluten','no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
-let item4 = new menuItem('Burger', '800', 'no', 'no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
-let item5 = new menuItem('BurgerSki', '900', 'gluten','no', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item = new menuItem('FireBurger', '700', 'Gluten and Lactose', 'https://www.mamoli.se/images/meny/hamburgare.png');
+let item2 = new menuItem('LegendBurger', '600', false , 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item3 = new menuItem('EpicBurger', '1000', 'Gluten', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item4 = new menuItem('Burger', '800', false, 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
+let item5 = new menuItem('BurgerSki', '900', 'Gluten', 'https://www.magnusjonasson.com/wp-content/2010/01/hamburger.png');
 
-document.getElementById("burger").innerHTML = item.info();
-document.getElementById("burger2").innerHTML = item2.info();
-document.getElementById("burger3").innerHTML = item3.info();
-document.getElementById("burger4").innerHTML = item4.info();
-document.getElementById("burger5").innerHTML = item5.info();
+let menu = [item,item2,item3,item4,item5];
 
-console.log(item.info());
-console.log(item2.info());
-console.log(item3.info());
-console.log(item4.info());
-console.log(item5.info());
+let myElement = document.getElementById("list");
+for (var i = 0 ; i < menu.length; i++)
+{
+	
+        let listItem = document.createElement("li");
+        let listValue = document.createTextNode(menu[i].name);
+       listItem.appendChild(listValue);
 
+       if(menu[i].allergis != false){
+      	   let listValue2 = document.createTextNode(' ' +menu[i].allergis);
+           listItem.appendChild(listValue2);
+       }
+    myElement.appendChild(listItem)
+         
+        
+        
+}
+ 
